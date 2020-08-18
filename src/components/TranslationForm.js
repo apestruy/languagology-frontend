e2675c5aa9155795776321b7c3c226c12b5ea9bf
Sasha <apestruy@gmail.com>
2020-08-18 01:16:02 -0400

    made form and connected API

--- src/components/TranslationForm.js
+++ src/components/TranslationForm.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { key } from "../constant";
+import { TextArea } from "../styled";
 
-const TranslationForm = (props) => {
-  return <div> TranslationForm </div>;
-};
+class TranslationForm extends React.Component {
+  state = {
+    input: "",
+    output: "",
+    languageId: "",
+  };
+
+  fetchApi = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `https://translation.googleapis.com/language/translate/v2?key=${key}`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          q: ["Hello world", "My name is Jeff"],
+          target: "de",
+        }),
+      }
+    )
+      .then((resp) => resp.json())
+      .then((translation) => console.log(translation));
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  saveTranslation = () => {
+    console.log("saved!");
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h2> Translate Here:</h2>
+        <div>
+          <form onChange={this.handleChange} onSubmit={this.fetchApi}>
+            <TextArea
+              name="input"
+              maxLength="75"
+              placeholder="Translate Text Here"
+            />
+            <TextArea name="output" disabled={true} />
+            <button type="submit">Translate</button>
+          </form>
+        </div>
+        <button onClick={this.saveTranslation}>Save Translation</button>
+      </div>
+    );
+  }
+}
 
 export default TranslationForm;
---
+++ src/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const TextArea = styled.textarea`
+  border-radius: 5px;
+  margin: 1rem;
+  font-size: 1.4em;
+  width: 25em;
+  height: 7em;
+`;
